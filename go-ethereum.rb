50a39c45d06aee7e3893fe07545dda724ac9526c
error2215 <slyvkamm@gmail.com>
2021-04-25 22:44:29 +0300

    test

---
+++ go-ethereum.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class GoEthereum < Formula
+  desc "Official Go implementation of the Ethereum protocol"
+  homepage "https://geth.ethereum.org"
+  url "https://github.com/error2215/go-ethereum/archive/v1.9.24.tar.gz"
+  sha256 "5e5d69dafc0c93af47fdc5fae782048b15a3a16ac722db39c9cda2eacdf21234"
+  license "LGPL-3.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test go-ethereum`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
